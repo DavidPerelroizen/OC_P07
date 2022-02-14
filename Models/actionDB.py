c3c68897de820b581cb3a35cd1e54974d3d19454
David Perelroizen <david.perelroizen@gmail.com>
2022-02-14 15:54:30 +0100

    Removed unnecessary argument from the extract method

--- Models/actionDB.py
+++ Models/actionDB.py
@@ -3,14 +3,13 @@ import csv
 
 
 class ActionDB:
-    def __init__(self, path, sheet_index):
+    def __init__(self, path):
         self.path = path
-        self.sheet_index = sheet_index
 
-    def extractdatacsv(self, path):
+    def extractdatacsv(self):
         """This method enables to extract the data from a csv file and save it as a list of Actions"""
         actions_list = []
-        with open(path, 'r') as csvfile:
+        with open(self.path, 'r') as csvfile:
             lines = csv.reader(csvfile, delimiter=',')
             for row in lines:
                 action = Action(row[0], row[1], row[2])
