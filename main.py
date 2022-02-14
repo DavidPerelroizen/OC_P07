ab549fc19b8bf60ce20036c2c341587a49b949ce
David Perelroizen <david.perelroizen@gmail.com>
2022-02-14 15:56:01 +0100

    Finalized the main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from View.views import View
+from Models.actionDB import ActionDB
+from Controller.bruteforce import findallcombinations, combinationranking
 
 
 def main():
-    pass
+    view = View()
+    path = "C:/Users/david/Documents/Openclassrooms/Python/projects/P07/OC_P07_actionDB_test.csv"
+
+    action_db = ActionDB(path)
+    actions_list = action_db.extractdatacsv()
+
+    combinations_list = findallcombinations(actions_list)
+    best_option = combinationranking(combinations_list)[0]
+
+    view.displaybestcombination(best_option)
+
 
 if __name__ == '__main__':
     main()
