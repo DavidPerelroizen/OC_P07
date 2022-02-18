28bfb51a51bd8ccbe78245d8a7bc6a28a83f8333
David Perelroizen <david.perelroizen@gmail.com>
2022-02-18 14:54:18 +0100

    Improved optimized algorithm

--- Controller/optimized.py
+++ Controller/optimized.py
@@ -1,11 +1,11 @@
 from Models.action import Action
-
+from Models.constants import max_spend_per_customer
 
 def sortactionslist(actions_list):
     actions_list_valuated = []
     for action in actions_list:
-        actions_list_valuated.append(action, action.finalvalue())
-    actions_list_valuated = sorted(actions_list_valuated, key=lambda x: x[1])
+        actions_list_valuated.append((action, action.finalvalue()))
+    actions_list_valuated = sorted(actions_list_valuated, key=lambda x: -x[1])
     return actions_list_valuated
 
 
@@ -14,15 +14,19 @@ def optimizedalgo(actions_list):
     # Step 1: sort the actions_list depending on each action valuated benefit
     actions_list_sorted = sortactionslist(actions_list)
 
-    # Step 2:
-    i = 0
+    # Step 2: find the best non-optimal combination by adding the most profitable actions from top to bottom
     profit_counter = 0
     spend_counter = 0
-    while spend_counter <= 500:
+    i = 0
+    while spend_counter <= max_spend_per_customer and i < len(actions_list):
         if actions_list_sorted[i][1] + profit_counter > profit_counter and \
-                actions_list_sorted[i][0].value + spend_counter <= 500:
+                actions_list_sorted[i][0].value + spend_counter <= max_spend_per_customer:
             best_option.append(actions_list_sorted[i])
             profit_counter += actions_list_sorted[i][1]
             spend_counter += actions_list_sorted[i][0].value
-        best_option.append()
+        i += 1
+    print(best_option)
+    print(spend_counter)
+    print(profit_counter)
+
     return best_option
