cbd843befe1bbb092c1c20e3121dcec47a34c1c7
David Perelroizen <david.perelroizen@gmail.com>
2022-02-18 15:08:21 +0100

    Updated main with the different possibilities

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from View.views import View
 from Models.actionDB import ActionDB
 from Controller.bruteforce import findallcombinations, combinationranking
+from Controller.optimized import optimizedalgo
 
 
 def main():
     view = View()
-    path = "csv_db/dataset1_Python+P7_test.csv"
+    path = "csv_db/dataset1_Python+P7.csv"
 
     action_db = ActionDB(path)
     actions_list = action_db.extractdatacsv()
 
-    combinations_list = findallcombinations(actions_list)
-    best_option = combinationranking(combinations_list)[0]
+    user_choice = view.displaymainmenu()
 
-    view.displaybestcombination(best_option)
+    if user_choice == 'B':
+        combinations_list = findallcombinations(actions_list)
+        best_option = combinationranking(combinations_list)[0]
+        view.displaybestcombination(best_option)
+
+    elif user_choice == 'O':
+        best_option = optimizedalgo(actions_list)
+
+    else:
+        exit()
 
 
 if __name__ == '__main__':
