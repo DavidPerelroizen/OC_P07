0370d7fc1ce4d8a446c79fee591e49e0fc34d18a
David Perelroizen <david.perelroizen@gmail.com>
2022-02-21 13:41:16 +0100

    Added a control to the database extract

--- Models/actionDB.py
+++ Models/actionDB.py
@@ -12,7 +12,7 @@ class ActionDB:
         with open(self.path, 'r') as csvfile:
             lines = csv.reader(csvfile, delimiter=',')
             for row in lines:
-                if lines.line_num == 1 or float(row[1]) == 0.0:
+                if lines.line_num == 1 or float(row[1]) == 0.0 or float(row[2]) == 0.0:
                     continue
                 action = Action(row[0], abs(float(row[1])), float(row[2])/100)  # Instantiate an Action for each row
                 actions_list.append(action)
